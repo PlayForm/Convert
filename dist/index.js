740b26933165e305d160bc187a78f9dadb4a83c1
Nikola Hristov <nikola@nikolahristov.tech>
2022-10-18 12:47:50 +0300

    Cleanup

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
-import{deepmerge as n}from"deepmerge-ts";import e from"./options/index.js";var h=(r={})=>{var a;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=e()[o]);const t=n(e(),r);return t.path=(a=t.path)!=null&&a.endsWith("/")?t.path:`${t.path}/`,{name:"astro-convert",hooks:{"astro:config:done":async o=>{t.path=t.path?t.path:o.config.outDir.toString()},"astro:build:done":async()=>{}}}};export{h as default};
+import { deepmerge as n } from "deepmerge-ts";
+import e from "./options/index.js";
+var h=(r={})=>{var a;for(const o in r)Object.prototype.hasOwnProperty.call(r,o)&&r[o]===!0&&(r[o]=e()[o]);const t=n(e(),r);return t.path=(a=t.path)!=null&&a.endsWith("/")?t.path:`${t.path}/`,{name:"astro-convert",hooks:{"astro:config:done":async o=>{t.path=t.path?t.path:o.config.outDir.toString()},"astro:build:done":async()=>{}}}};export { h as default };
+
