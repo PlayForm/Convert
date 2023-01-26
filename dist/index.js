aadae9f51578fd41ef4ff8edce81179e7c8eeac7
Nikola Hristov <nikola@nikolahristov.tech>
2023-01-26 20:03:52 +0200

    squash!

--- dist/index.js
+++ dist/index.js
@@ -1,1 +1,4 @@
-import{deepmerge as a}from"deepmerge-ts";import e from"./options/index.js";var i=(t={})=>{for(const r in t)Object.prototype.hasOwnProperty.call(t,r)&&t[r]===!0&&(t[r]=e()[r]);const o=a(e(),t);return o.path=o.path?.endsWith("/")?o.path:`${o.path}/`,{name:"astro-convert",hooks:{"astro:build:done":async()=>{}}}};export{i as default};
+import { deepmerge as a } from "deepmerge-ts";
+import e from "./options/index.js";
+var i=(t={})=>{for(const r in t)Object.prototype.hasOwnProperty.call(t,r)&&t[r]===!0&&(t[r]=e()[r]);const o=a(e(),t);return o.path=o.path?.endsWith("/")?o.path:`${o.path}/`,{name:"astro-convert",hooks:{"astro:build:done":async()=>{}}}};export { i as default };
+
