2162e4dec4905f1c67a4c6827b66b403997d552e
Nikola Hristov <nikola@nikolahristov.tech>
2023-12-17 07:57:18 +0200

    squash!

--- Target/Function/Integration.js
+++ Target/Function/Integration.js
@@ -1,1 +1,23 @@
-var f=(...[e={}])=>{for(const t in e)Object.prototype.hasOwnProperty.call(e,t)&&e[t]===!0&&(e[t]=o[t]);const{Path:a}=s(o,e),r=new Set;if(typeof a<"u"&&(Array.isArray(a)||a instanceof Set))for(const t of a)r.add(t);return{name:"astro-convert",hooks:{"astro:build:done":async({dir:t})=>{r.size||r.add(t)}}}};const{default:o}=await import("../Variable/Option.js"),{default:s}=await import("typescript-esbuild/Target/Function/Merge.js");export{o as Default,s as Merge,f as default};
+var f = (...[e = {}]) => {
+	for (const t in e)
+		Object.prototype.hasOwnProperty.call(e, t) &&
+			e[t] === !0 &&
+			(e[t] = o[t]);
+	const { Path: a } = s(o, e),
+		r = new Set();
+	if (typeof a < "u" && (Array.isArray(a) || a instanceof Set))
+		for (const t of a) r.add(t);
+	return {
+		name: "astro-convert",
+		hooks: {
+			"astro:build:done": async ({ dir: t }) => {
+				r.size || r.add(t);
+			},
+		},
+	};
+};
+const { default: o } = await import("../Variable/Option.js"),
+	{ default: s } = await import(
+		"typescript-esbuild/Target/Function/Merge.js"
+	);
+export { o as Default, s as Merge, f as default };
